61c09f048cb707402c7e9970987d6d43f5199edc
gabe8135 <70823529+gabe8135@users.noreply.github.com>
2025-07-03 13:11:31 -0300

    implementaçao de analytics novamente sob novos parametros

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX');
+              gtag('config', 'G-792WGGT77D');
             `,
           }}
         />
