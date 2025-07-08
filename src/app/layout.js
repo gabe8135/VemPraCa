82e0cdab7a4c65c5cfdcb030200c01f868e82a43
gabe8135 <70823529+gabe8135@users.noreply.github.com>
2025-07-08 00:23:33 -0300

    concerto em um pequeno erro

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import AOSInit from "./components/AOSInit";
+// import AOSInit from "./components/AOSInit";
 
 // Declare as fontes aqui, todas juntas
 const inter = Inter({
@@ -54,7 +54,7 @@ export default function RootLayout({ children }) {
       className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} ${playfair.variable} ${montserrat.variable}`}
     >
       <head>
-        <AOSInit />
+        {/* <AOSInit /> */}
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-792WGGT77D"></script>
         <script
